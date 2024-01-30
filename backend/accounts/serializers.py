a7703c32e3c2503bdf319aeb5c4768bfff024e75
lasha-chipashvili-github <146829747+lasha-chipashvili-github@users.noreply.github.com>
2024-01-30 08:13:42 +0400

    Delete backend directory

--- backend/accounts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from dj_rest_auth.serializers import LoginSerializer
-
-class CustomLoginSerializer(LoginSerializer):
-    username = None
-
-
--- backend/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-# from .views import api_login,  api_logout
-
-urlpatterns = [
-]
